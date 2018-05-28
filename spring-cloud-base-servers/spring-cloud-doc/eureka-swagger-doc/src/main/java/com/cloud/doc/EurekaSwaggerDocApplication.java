f869695a0f54880d6336d4ddccea8505595e9b48
xieshengrong <xieshengrong@homedo.com>
2018-05-28 15:32:45 +0800

    依赖

--- spring-cloud-base-servers/spring-cloud-doc/eureka-swagger-doc/src/main/java/com/cloud/doc/EurekaSwaggerDocApplication.java
+++ spring-cloud-base-servers/spring-cloud-doc/eureka-swagger-doc/src/main/java/com/cloud/doc/EurekaSwaggerDocApplication.java
@@ -4,11 +4,13 @@ import com.cloud.butler.configure.EnableSwaggerButler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /** @author xieshengrong */
 @EnableDiscoveryClient
 @EnableSwaggerButler
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.cloud.butler"})
 public class EurekaSwaggerDocApplication {
 
     public static void main(String[] args) {
